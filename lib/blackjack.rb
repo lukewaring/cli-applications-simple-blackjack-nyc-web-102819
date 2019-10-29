2e3d2b05b8d2e0130f13961ee41a8c447097b160
Luke Waring <lukewaring@gmail.com>
2019-10-29 17:46:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,17 +62,12 @@ def runner
   card_sum = initial_round
   
   until card_sum > 21 do
-<<<<<<< HEAD
     card_sum = hit?(card_sum)
     display_card_total(card_sum)
   end
   
   end_game(card_sum)
-=======
       
-  end
-  
->>>>>>> 7d202d4b5ffbc0c6da065280d927bb2531db8b8c
 end
 
 # 1. Welcome the user
